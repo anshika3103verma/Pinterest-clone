d1dfc4644bd604c03d768fba4d2e2ed34813b0ae
Anshika Verma <anshika3103verma@gmail.com>
2026-03-06 20:53:41 +0530

    fix: use MONGO_URI env variable

--- models/users.js
+++ models/users.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/project1");
-
+// ✅ Correct - uses env variable
+mongoose.connect(process.env.MONGO_URI)
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
